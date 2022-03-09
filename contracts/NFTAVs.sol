98d42bc0bf9dad72ed1d5316b56fe01aa68767d3
HenryFanDi <abcdz330@gmail.com>
2022-03-09 18:35:27 +0800

    Update: Contract to `ERC721A`

--- contracts/NFTAVs.sol
+++ contracts/NFTAVs.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTAVs is ERC721, ERC721URIStorage, Ownable {
+contract NFTAVs is ERC721A, Ownable {
+
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -14,29 +15,34 @@ contract NFTAVs is ERC721, ERC721URIStorage, Ownable {
     // Make sure that the same uri can't be minted twice.
     mapping(string => uint8) existingURIs;
 
-    constructor() ERC721("NFTAVs", "NAV") {}
+    constructor() ERC721A("NFTAVs", "NAV") {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://";
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function mint(uint256 quantity) external payable {
+        require(msg.value >= (0.05 ether * quantity), 'Need to pay up!');
+        bool isExceed = _totalMinted() + quantity > 10;
+        require(!isExceed, 'Is exceed total supply!');
+        
+        // _safeMint's second argument now takes in a quantity, not a tokenId.
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        _safeMint(msg.sender, quantity);
+        tokenURI(tokenId);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId) internal override {
         super._burn(tokenId);
     }
 
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override
         returns (string memory)
     {
         return super.tokenURI(tokenId);
@@ -48,21 +54,22 @@ contract NFTAVs is ERC721, ERC721URIStorage, Ownable {
 
     // It takes a recipient and metadata content id then mint a new token with that data.
     // `payable` : means that somebody can transfer money into the contract.
+    // TODO: [metadataURL] array.
     function payToMint(
-        address recipient,
-        string memory metadataURI
-    ) public payable returns (uint256) {
+        string memory metadataURI,
+        uint256 quantity
+    ) public payable {
         require(existingURIs[metadataURI] != 1, 'NFT already minted!');
         // Make sure that the minimum value is greater than xxx.
-        require(msg.value >= 0.05 ether, 'Need to pay up!');
+        require(msg.value >= (0.05 ether * quantity), 'Need to pay up!');
 
         uint256 newItemId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         existingURIs[metadataURI] = 1;
 
         // Minted!
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, metadataURI);
+        _safeMint(msg.sender, quantity);
+        tokenURI(newItemId);
     }
 
     // Know exactly how many tokens have been minted.
