6aeab69ff157727555ff6eb327a19c9bb1b489bd
HenryFanDi <abcdz330@gmail.com>
2022-03-09 11:40:23 +0800

    Update: `NFTAVs` testing

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,39 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("NFTAVs", function () {
+  it("Should mint and transfer an NFT to someone", async function () {
+    const NFTAVs = await ethers.getContractFactory("NFTAVs");
+    const nftAVs = await NFTAVs.deploy();
+    await nftAVs.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const recipient = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
+    const metadataURI = 'cid/test.png';
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    // Check the current balance of the recipient's wallet address balance of is a method built into the ERC721 spec,
+    // now currently the user hasn't purchased any nfts, so we expect the balance to equal zero.
+    let balance = await nftAVs.balanceOf(recipient);
+    expect(balance).to.equal(0);
+
+    // Newly minted token that calls the payment method that we defined on the smart contract,
+    // it takes the recipient and metadata uri as argument and then we can also define the message value as 0.05 ether.
+    const newlyMintedToken = await nftAVs.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther('0.05') });
 
     // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await newlyMintedToken.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    balance = await nftAVs.balanceOf(recipient);
+    expect(balance).to.equal(1);
+
+    expect(await nftAVs.isContentOwned(metadataURI)).to.equal(true);
+
+    // expect(await greeter.greet()).to.equal("Hello, world!");
+
+    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait();
+
+    // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
